137f174d644e65e543c19c8c5951c96b36747a1e
Adityadwiagung <adityadwiagung@gmail.com>
2023-09-15 10:25:58 +0700

    menambah tugas prioritas 1

---
+++ Minggu 4/Soal Platform Widget Layout/Soal Prioritas 1/flutter_1/lib/contact_model.dart
@@ -0,0 +1,46 @@
+class Contact {
+  final String username;
+  final String phone;
+
+  Contact({
+    required this.username,
+    required this.phone,
+  });
+}
+
+List<Contact> contactList = [
+  Contact(
+    username: "PurplePenguin22", 
+    phone: "(555) 123-4567",
+  ),
+  Contact(
+    username: "GreenGiraffe99", 
+    phone: "(555) 234-5678",
+  ),
+  Contact(
+    username: "SilverSunshine11", 
+    phone: "(555) 345-6789",
+  ),
+  Contact(
+    username: "BlueButterfly44", 
+    phone: "(555) 456-7890",
+  ),
+  Contact(
+    username: "GoldenDragonfly77", 
+    phone: "(555) 567-8901",
+  ),
+  Contact(
+    username: "RedRainbow66", 
+    phone: "(555) 678-9012",
+  ),
+  Contact(
+    username: "OrangeMeadow55", 
+    phone: "(555) 789-0123",
+  ),
+  Contact(
+    username: "YellowNightfall33", 
+    phone: "(555) 890-1234",
+  ),
+  
+  
+];
