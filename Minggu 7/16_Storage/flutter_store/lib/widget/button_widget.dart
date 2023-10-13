9d34ada34c98ffa120dc54e0e9b6bddba436cb7d
Adityadwiagung <adityadwiagung@gmail.com>
2023-10-13 15:48:47 +0700

    menambahkan tugas storage

---
+++ Minggu 7/16_Storage/flutter_store/lib/widget/button_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_storage/theme/theme_color.dart';
+import 'package:flutter_storage/theme/theme_text_style.dart';
+
+class ButtonWidget extends StatelessWidget {
+  final String title;
+  final Function()? onPressed;
+
+
+  const ButtonWidget({super.key, required this.title, required this.onPressed,});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: ThemeColor().m3SysLightPurple,
+      ),
+      onPressed: onPressed,
+      child: Text(
+        title,
+        style: ThemeTextStyle().m3LabelLarge,
+      )
+    );
+  }
+}
